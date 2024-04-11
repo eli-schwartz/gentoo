eli-schwartz/gentoo

commit 3e0d35af149c5a90556fd655456a2ba4e25fbaee

net-fs/curlftpfs: modernize to EAPI=7
Package-Manager: Portage-2.3.76, Repoman-2.3.17
Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

net-fs/curlftpfs/curlftpfs-0.9.2-r4.ebuild
@@ -1,9 +1,9 @@
-# Copyright 1999-2018 Gentoo Foundation
+# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=5
+EAPI=7
 
-inherit eutils autotools
+inherit autotools
 
 DESCRIPTION="File system for accessing ftp hosts based on FUSE"
 HOMEPAGE="http://curlftpfs.sourceforge.net/"
@@ -21,17 +21,19 @@ RDEPEND=">=net-misc/curl-7.17.0
 DEPEND="${RDEPEND}
 	virtual/pkgconfig"
 
+PATCHES=(
+	"${FILESDIR}"/${P}-64bit_filesize.patch
+	"${FILESDIR}"/${PN}-0.9.2-darwin.patch
+	"${FILESDIR}"/${PN}-0.9.2-memleak.patch
+	"${FILESDIR}"/${PN}-0.9.2-memleak-nocache.patch
+	"${FILESDIR}"/${PN}-0.9.2-fix-escaping.patch
+)
+
 src_prepare() {
-	epatch "${FILESDIR}"/${P}-64bit_filesize.patch
-	epatch "${FILESDIR}"/${PN}-0.9.2-darwin.patch
-	epatch "${FILESDIR}"/${PN}-0.9.2-memleak.patch
-	epatch "${FILESDIR}"/${PN}-0.9.2-memleak-nocache.patch
-	epatch "${FILESDIR}"/${PN}-0.9.2-fix-escaping.patch
+	default
 
 	# automake-1.13.1 obsoletes AM_* bit #469818
 	sed -i -e 's/AM_CONFIG_HEADER/AC_CONFIG_HEADERS/' configure.ac || die
-
-	epatch_user
 
 	eautoreconf
 }

net-fs/curlftpfs/files/curlftpfs-0.9.2-64bit_filesize.patch
@@ -1,5 +1,5 @@
---- ftpfs.c.old	2009-11-28 14:28:05.000000000 +0300
-+++ ftpfs.c	2009-11-28 14:28:19.000000000 +0300
+--- a/ftpfs.c	2009-11-28 14:28:05.000000000 +0300
++++ b/ftpfs.c	2009-11-28 14:28:19.000000000 +0300
 @@ -503,7 +503,7 @@
    
    curl_easy_setopt_or_die(fh->write_conn, CURLOPT_URL, fh->full_path);

net-fs/curlftpfs/files/curlftpfs-0.9.2-darwin.patch
@@ -1,8 +1,7 @@
 https://sourceforge.net/tracker/?func=detail&atid=816359&aid=2799820&group_id=160565
 
-diff -ur configure.ac configure.ac
---- configure.ac	2008-04-23 15:37:42 +0400
-+++ configure.ac	2009-06-02 09:46:05 +0400
+--- a/configure.ac	2008-04-23 15:37:42 +0400
++++ b/configure.ac	2009-06-02 09:46:05 +0400
 @@ -18,6 +18,11 @@
  	AC_MSG_ERROR(["We need libcurl with support for FTP protocol."])
  fi

commit 0739a08fa6a4c107bfb4c654a2ea5e623e5827d4

net-fs/curlftpfs: fix DEPEND after port to EAPI 7
During the migration to EAPI 7, pkgconfig should have been moved to
BDEPEND, as DEPEND is no longer installed for both.

Fixes: 3e0d35af149c5a90556fd655456a2ba4e25fbaee
Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>

net-fs/curlftpfs/curlftpfs-0.9.2-r4.ebuild
@@ -14,11 +14,13 @@ SLOT="0"
 KEYWORDS="amd64 arm ~arm64 x86 ~amd64-linux ~x86-linux"
 RESTRICT="test" # bug 258460
 
-RDEPEND=">=net-misc/curl-7.17.0
+RDEPEND="
+	>=net-misc/curl-7.17.0
 	>=sys-fs/fuse-2.2:0=
-	>=dev-libs/glib-2.0"
-DEPEND="${RDEPEND}
-	virtual/pkgconfig"
+	>=dev-libs/glib-2.0
+"
+DEPEND="${RDEPEND}"
+BDEPEND="virtual/pkgconfig"
 
 PATCHES=(
 	"${FILESDIR}"/${P}-64bit_filesize.patch